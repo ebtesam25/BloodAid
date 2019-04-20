b3e16aff984f5481be5c26704ad6681364c132d4
ebtesam25 <ebtesam@iut-dhaka.edu>
2019-04-20 13:35:50 +0600

    Authentication done

--- app/src/main/java/com/ebtesam/iutbloodaid/LoginActivity.java
+++ app/src/main/java/com/ebtesam/iutbloodaid/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.ebtesam.iutbloodaid;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,22 +9,29 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements OnClickListener {
     private EditText emailEditText,passwordEditText;
     private Button forgotPassword;
     private ImageButton logIn,signUp;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        this.setTitle("Sign In");
 
         emailEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
         forgotPassword = findViewById(R.id.forgotPass);
         logIn = findViewById(R.id.loginButton);
         signUp = findViewById(R.id.signupButton);
+        mAuth = FirebaseAuth.getInstance();
 
         signUp.setOnClickListener(this);
         forgotPassword.setOnClickListener(this);
@@ -38,15 +46,15 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
 
         switch (v.getId()){
 
-            case R.id.loginButton
+            case R.id.loginButton:
                 userLogIn();
                 break;
 
-            case R.id.forgotPass
+            case R.id.forgotPass:
 
                 break;
 
-            case R.id.signupButton
+            case R.id.signupButton:
                 Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
                 startActivity(intent);
                 break;
@@ -67,25 +75,48 @@ public class LoginActivity extends AppCompatActivity implements OnClickListener 
             emailEditText.requestFocus();
             return;
         }
+        else if(!email.endsWith("iut-dhaka.edu")){
+            emailEditText.setError("Invalid E-mail Address");
+            emailEditText.requestFocus();
+            return;
+        }
 
-        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             emailEditText.setError("Enter a valid Email Address");
-
             emailEditText.requestFocus();
             return;
 
+
         }
-        if(password.isEmpty()){
+        else if(password.isEmpty()){
             passwordEditText.setError("Enter your password");
             passwordEditText.requestFocus();
             return;
         }
-        if(password.length()<6){
+        else if(password.length()<6){
             passwordEditText.setError("Minimum length should be 6");
             passwordEditText.requestFocus();
             return;
 
         }
+        else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent mainIntent = new Intent(LoginActivity.this, ChooseActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(LoginActivity.this, "Invalid E-mail/Password combination", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
 
 
     }
