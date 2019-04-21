caba6b02fab39e069fd52e277db93cf7d6e7cb82
Mumtahin Habib <38589844+mhrafi66@users.noreply.github.com>
2019-04-21 07:55:33 +0600

    Search Activity done.
    Add to firebase and then show results

--- app/src/main/java/com/ebtesam/iutbloodaid/SearchActivity.java
+++ app/src/main/java/com/ebtesam/iutbloodaid/SearchActivity.java
@@ -2,12 +2,43 @@ package com.ebtesam.iutbloodaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SearchActivity extends AppCompatActivity {
-
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
+    String email,password;
+    String bg[];
+    String bldgrp;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        this.setTitle("Search For Blood");
+        Bundle bundle=getIntent().getExtras();
+        if(bundle!=null){
+            email=bundle.getString("email");
+            password=bundle.getString("pass");
+        }
+        bg=getResources().getStringArray(R.array.blood_groups);
+
+        spinner=findViewById(R.id.bg);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_custom,R.id.custom_spinner,bg);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.bg){
+            bldgrp=spinner.getSelectedItem().toString();
+        }
     }
 }
